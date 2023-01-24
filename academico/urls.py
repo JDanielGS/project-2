db7450843892acd130d64fdfd778a20112e54212
JDanielGS <danielgachasut@gmail.com>
2023-01-24 18:47:43 -0500

    version 1.2 pag uni con django

--- academico/urls.py
+++ academico/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import home, RegistrarCurso
+from .views import home, RegistrarCurso, EliminarCurso, EdicionCurso, EditarCurso
 
 urlpatterns = [
     path('', home),
     path('registrarCurso/', RegistrarCurso),
+    path('eliminarCurso/<codigo>', EliminarCurso),
+    path('editarCurso/', EditarCurso),
+    path('edicionCurso/<codigo>', EdicionCurso),
 ]
--- academico/views.py
+++ academico/views.py
@@ -13,3 +13,27 @@ def RegistrarCurso(request):
     Curso=curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
 
     return redirect('/')
+
+def EliminarCurso(request, codigo):
+    Curso=curso.objects.get(codigo=codigo)
+
+    Curso.delete()
+
+    return redirect('/')
+
+def EdicionCurso(request, codigo):
+    Curso=curso.objects.get(codigo=codigo)
+    return render(request, "edicion_curso.html", {"curso":Curso})
+
+def EditarCurso(request):
+    codigo=request.POST["txtCOD"]
+    nombre=request.POST["txtNOM"]
+    creditos=request.POST["txtCRED"]
+
+    Curso=curso.objects.get(codigo=codigo)
+    Curso.nombre = nombre
+    Curso.creditos = creditos
+
+    Curso.save()
+
+    return redirect('/')

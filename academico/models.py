bcbf5b89092b0c3b459b7c12d9b4c938a48bb6cd
JDanielGS <danielgachasut@gmail.com>
2023-01-24 16:54:03 -0500

    version 1.0 pag uni con django

--- academico/models.py
+++ academico/models.py
@@ -4,3 +4,7 @@ class curso(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
     nombre=models.CharField(max_length=50)
     creditos=models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        texto="{0} ({1})"
+        return texto.format(self.nombre, self.creditos)
